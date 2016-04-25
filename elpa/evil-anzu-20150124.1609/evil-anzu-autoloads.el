8ed5fab148cae34b811d10d7877dedfe80b047c1
wuguojun <wuguojun.wu@huawei.com>
2016-04-25 11:02:58 +0800

    add the dotfile and some packages

---
+++ elpa/evil-anzu-20150124.1609/evil-anzu-autoloads.el
@@ -0,0 +1,15 @@
+;;; evil-anzu-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("evil-anzu.el") (22266 10637 440536 652000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; evil-anzu-autoloads.el ends here
---
+++ elpa/parent-mode-20150824.1600/parent-mode-autoloads.el
@@ -0,0 +1,16 @@
+;;; parent-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("parent-mode.el") (22266 10673 833511
+;;;;;;  571000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; parent-mode-autoloads.el ends here
